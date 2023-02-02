72c22cf7d260fb842e03247297036121edff3ce6
Samuel P <spinillos@wawand.co>
2023-02-02 17:36:57 -0500

    feat: adding ability to override default classes

--- form_test.go
+++ form_test.go
@@ -1,10 +1,12 @@
 package tailush_test
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
-	"tailush"
 	"testing"
+
+	"github.com/wawandco/tailush"
 
 	"github.com/gobuffalo/plush/v4"
 	"github.com/gobuffalo/tags/v3"
@@ -110,6 +112,28 @@ func TestLabel(t *testing.T) {
 			t.Fatalf("form should contain mixed classes")
 		}
 	})
+
+	t.Run("overriding default classes", func(t *testing.T) {
+		opt := tailush.UseLabelClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		l := f.Label("any", tags.Options{})
+		if !strings.Contains(string(l.HTML()), `<label class="my-custom-class">any</label>`) {
+			t.Fatalf("form shouldn't contain mixed classes")
+		}
+	})
+
+	t.Run("overriding default and passing classes", func(t *testing.T) {
+		opt := tailush.UseLabelClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		l := f.Label("any", tags.Options{"class": "my-other-class"})
+		if !strings.Contains(string(l.HTML()), `<label class="my-custom-class my-other-class">any</label>`) {
+			t.Fatalf("form should contain mixed classes")
+		}
+	})
 }
 
 func TestFileTag(t *testing.T) {
@@ -124,7 +148,28 @@ func TestFileTag(t *testing.T) {
 		if !strings.Contains(string(f.HTML()), `enctype="multipart/form-data"`) {
 			t.Fatalf("enctype should be multipart/form-data")
 		}
+	})
 
+	t.Run("overriding default classes", func(t *testing.T) {
+		opt := tailush.UseFileClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ft := f.FileTag(tags.Options{})
+		if !strings.Contains(string(ft.HTML()), `<input class="my-custom-class" type="file" />`) {
+			t.Fatalf("form shouldn't contain mixed classes")
+		}
+	})
+
+	t.Run("overriding default and passing classes", func(t *testing.T) {
+		opt := tailush.UseFileClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ft := f.FileTag(tags.Options{"class": "my-other-class"})
+		if !strings.Contains(string(ft.HTML()), `<input class="my-custom-class my-other-class" type="file" />`) {
+			t.Fatalf("form should contain mixed classes")
+		}
 	})
 }
 
@@ -174,6 +219,28 @@ func TestTextArea(t *testing.T) {
 			t.Fatalf("textarea should not contain tag_only")
 		}
 	})
+
+	t.Run("overriding default classes", func(t *testing.T) {
+		opt := tailush.UseTextAreaClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ta := f.TextAreaTag(tags.Options{})
+		if !strings.Contains(string(ta.HTML()), `<textarea class="my-custom-class"></textarea>`) {
+			t.Fatalf("form shouldn't contain mixed classes")
+		}
+	})
+
+	t.Run("overriding default and passing classes", func(t *testing.T) {
+		opt := tailush.UseTextAreaClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ta := f.TextAreaTag(tags.Options{"class": "my-other-class"})
+		if !strings.Contains(string(ta.HTML()), `<textarea class="my-custom-class my-other-class"></textarea>`) {
+			t.Fatalf("form should contain mixed classes")
+		}
+	})
 }
 
 func TestHiddenTag(t *testing.T) {
@@ -183,6 +250,103 @@ func TestHiddenTag(t *testing.T) {
 
 		if !strings.Contains(string(f.HTML()), `<input name="id" type="hidden" value="1" />`) {
 			t.Fatalf("textarea should not contain tag_only")
+		}
+	})
+}
+
+func TestInputTag(t *testing.T) {
+	t.Run("overriding default classes", func(t *testing.T) {
+		opt := tailush.UseInputClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		it := f.InputTag(tags.Options{})
+		if !strings.Contains(string(it.HTML()), `<input class="my-custom-class" type="text" />`) {
+			t.Fatalf("form shouldn't contain mixed classes")
+		}
+	})
+
+	t.Run("overriding default and passing classes", func(t *testing.T) {
+		opt := tailush.UseInputClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		it := f.InputTag(tags.Options{"class": "my-other-class"})
+		if !strings.Contains(string(it.HTML()), `<input class="my-custom-class my-other-class" type="text" />`) {
+			t.Fatalf("form should contain mixed classes")
+		}
+	})
+}
+
+func TestDateTimeTag(t *testing.T) {
+	t.Run("overriding default classes", func(t *testing.T) {
+		opt := tailush.UseDateInputClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		dt := f.DateTimeTag(tags.Options{})
+		if !strings.Contains(string(dt.HTML()), `<input class="my-custom-class" format="2006-01-02T03:04" type="datetime-local" />`) {
+			t.Fatalf("form shouldn't contain mixed classes")
+		}
+	})
+
+	t.Run("overriding default and passing classes", func(t *testing.T) {
+		opt := tailush.UseDateInputClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		dt := f.DateTimeTag(tags.Options{"class": "my-other-class"})
+		if !strings.Contains(string(dt.HTML()), `<input class="my-custom-class my-other-class" format="2006-01-02T03:04" type="datetime-local" />`) {
+			t.Fatalf("form should contain mixed classes")
+		}
+	})
+}
+
+func TestCheckboxTag(t *testing.T) {
+	t.Run("overriding default classes", func(t *testing.T) {
+		opt := tailush.UseCheckboxClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ct := f.CheckboxTag(tags.Options{})
+		if !strings.Contains(string(ct.HTML()), `<input class="my-custom-class" type="checkbox" value="true" />`) {
+			t.Fatalf("form shouldn't contain mixed classes")
+		}
+	})
+
+	t.Run("overriding default and passing classes", func(t *testing.T) {
+		opt := tailush.UseCheckboxClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ct := f.CheckboxTag(tags.Options{"class": "my-other-class"})
+		if !strings.Contains(string(ct.HTML()), `<input class="my-custom-class my-other-class" type="checkbox" value="true" />`) {
+			t.Fatalf("form should contain mixed classes")
+		}
+	})
+}
+
+func TestRadioButtonTag(t *testing.T) {
+	t.Run("overriding default classes", func(t *testing.T) {
+		opt := tailush.UseRadioClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ct := f.RadioButtonTag(tags.Options{})
+		fmt.Println(string(ct.HTML()))
+		if !strings.Contains(string(ct.HTML()), `<input class="my-custom-class" type="radio" checked />`) {
+			t.Fatalf("form shouldn't contain mixed classes")
+		}
+	})
+
+	t.Run("overriding default and passing classes", func(t *testing.T) {
+		opt := tailush.UseRadioClass("my-custom-class")
+		f := tailush.NewForm(tags.Options{}, newHctx())
+		opt(f)
+
+		ct := f.RadioButtonTag(tags.Options{"class": "my-other-class"})
+		if !strings.Contains(string(ct.HTML()), `<input class="my-custom-class my-other-class" type="radio" checked />`) {
+			t.Fatalf("form should contain mixed classes")
 		}
 	})
 }
--- options.go
+++ options.go
@@ -2,20 +2,72 @@ package tailush
 
 type FormHelperOption func(*form)
 
+// UseFieldContainerClass is used to override the default classes for the container element.
 func UseFieldContainerClass(class string) FormHelperOption {
 	return func(f *form) {
 		f.fieldContainerClass = class
 	}
 }
 
+// UseLabelClass is used to override the default classes for the label element.
 func UseLabelClass(class string) FormHelperOption {
 	return func(f *form) {
 		f.labelClass = class
 	}
 }
 
+// UseInputClass is used to override the default classes for the input element.
 func UseInputClass(class string) FormHelperOption {
 	return func(f *form) {
 		f.inputClass = class
+	}
+}
+
+// UseCheckboxClass is used to override the default classes for the checkbox element.
+func UseCheckboxClass(class string) FormHelperOption {
+	return func(f *form) {
+		f.checkboxClass = class
+	}
+}
+
+// UseRadioClass is used to override the default classes for the radio element.
+func UseRadioClass(class string) FormHelperOption {
+	return func(f *form) {
+		f.radioClass = class
+	}
+}
+
+// UseFileClass is used to override the default classes for the file element.
+func UseFileClass(class string) FormHelperOption {
+	return func(f *form) {
+		f.fileClass = class
+	}
+}
+
+// UseTextAreaClass is used to override the default classes for the textarea element.
+func UseTextAreaClass(class string) FormHelperOption {
+	return func(f *form) {
+		f.textAreaClass = class
+	}
+}
+
+// UseSelectClass is used to override the default classes for the select element.
+func UseSelectClass(class string) FormHelperOption {
+	return func(f *form) {
+		f.selectClass = class
+	}
+}
+
+// UseErrorClass is used to override the default classes for the errors.
+func UseErrorClass(class string) FormHelperOption {
+	return func(f *form) {
+		f.errorClass = class
+	}
+}
+
+// UseDateInputClass is used to override the default classes for the errors.
+func UseDateInputClass(class string) FormHelperOption {
+	return func(f *form) {
+		f.dateInputClass = class
 	}
 }
