612d72d139e0ec6958df8f59205413db6c463dba
Samuel P <spinillos@wawand.co>
2023-02-03 13:59:52 -0500

    test: added test scenarios for formFor input type="hidden"

---
+++ formfor_test.go
@@ -0,0 +1,31 @@
+package tailush_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gobuffalo/tags/v3"
+	"github.com/wawandco/tailush"
+)
+
+func TestFormForInputTag(t *testing.T) {
+	t.Run("type hidden", func(t *testing.T) {
+		var model struct{}
+		f := tailush.NewFormFor(model, tags.Options{}, newHctx())
+
+		it := f.InputTag("", tags.Options{
+			"type": "hidden",
+		})
+
+		fmt.Println(string(it.HTML()))
+
+		if strings.Contains(string(it.HTML()), `<label class="block text-sm font-medium text-gray-700 mb-1">Name</label>`) {
+			t.Fatalf("form shouldn't contain label")
+		}
+
+		if !strings.Contains(string(it.HTML()), `<input id="-" name="" tags-field="" type="hidden" value="" />`) {
+			t.Fatalf("form should contain hidden input")
+		}
+	})
+}
